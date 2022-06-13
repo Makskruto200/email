87c3e4429d0b27bcb4c2a35769471c9e2ecc8ee0
Makskruto200 <104332192+Makskruto200@users.noreply.github.com>
2022-06-13 11:24:55 +0700

    Подключение js к интерфейсу пользователя

--- готовый/js/app_user.js
+++ готовый/js/app_user.js
@@ -1,1 +1,35 @@
-&
+$(function() {
+    
+ 
+    $("#text").text(localStorage.getItem("op"))
+    $("body").css("background", localStorage.getItem("fon"))
+    $("body").css("backgroundSize","cover")
+        
+       
+    
+    $("#fon").click(function() {
+    
+        $("#submenu").slideToggle(500);
+    });
+    
+
+$(".mun").click(function() {
+        localStorage.setItem("fon",$(this).css("background"));
+        $("body").css("background",$(this).css("background"))
+        $("body").css("backgroundSize","cover")
+        
+      
+        
+    });
+    $("#_").click(function() {
+        
+        $("#text_input").slideToggle(500);
+    });
+    
+    
+ });
+    input=document.getElementById("text_input")
+    input.oninput = function() {
+        localStorage.setItem("op",input.value);
+       $(function() {$("#text").text(input.value)})
+  };
