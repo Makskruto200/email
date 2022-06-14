b2e279e98235b2efc7faed6583c2160711d8343a
Makskruto200 <104332192+Makskruto200@users.noreply.github.com>
2022-06-14 13:54:39 +0700

    Добавления обработчика для отсылания изображений

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 #_______________Импорты________________
 from flask import Flask,render_template,url_for,request,flash,redirect, session
 import sql
-
+from datetime import datetime
+from werkzeug.utils import secure_filename
+import os
 #_______Регистрация приложения_______
 app=Flask(__name__)
 app.config.from_object(__name__)
 app.config["SECRET_KEY"]="djsenrbbrvrjsiskssnbeehrhejsn"
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+UPLOAD_FOLDER = './static/file'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 #__Ознакомление с email___
 @app.route("/")
 def index():
@@ -28,7 +33,7 @@ def login():
 #Пользователи
 @app.route("/user/",methods=["POST","GET"])
 def user():
-    return f"{session['email']}"
+    return render_template("user.html",email=session["email"])
     
 
 
@@ -43,9 +48,27 @@ def create():
         
     return render_template("create.html")
 
+@app.route("/user/send",methods=["POST","GET"])
+def user_send():
+    if request.method=="POST":
+        sql.letter(
+        session['email'],
+        request.form["email_2"],
+        request.form["text"],
+        request.files["file_1"].filename,
+        request.files["file_2"].filename,
+        request.files["file_3"].filename,
+        request.files["file_4"].filename,
+        request.files["file_5"].filename,
+        datetime.now()
+        )
+        file = request.files['file_1']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+    return render_template("send.html")
 
    
-      
+ 
          
             
                
