3ef7f3efc1353f22dea31a7f845f710f774637ae
Makskruto200 <104332192+Makskruto200@users.noreply.github.com>
2022-06-14 13:55:39 +0700

    Добавления метода для написания письма в бд

--- sql.py
+++ sql.py
@@ -1,4 +1,5 @@
 import sqlite3 as sq
+from datetime import datetime
 def authorization_account(email, password):
    try:
             with sq.connect("users.db") as con:
@@ -17,5 +18,12 @@ def create_account(email,password):
                 cur=con.cursor()
                 cur.execute(f"""INSERT INTO  users (email, password) VALUES ("{email}","{password}")""")
     except:
+       pass
+def letter(email,email_2,text,file_1,file_2,file_3,file_4,file_5,data):
+   try:
+            with sq.connect("users.db") as con:
+                cur=con.cursor()
+                cur.execute(f"""INSERT INTO  letter VALUES ("{email}","{email_2}","{text}","{file_1}","{file_2}","{file_3}","{file_4}","{file_5}","{data}")""")
+   except:
        pass
    
